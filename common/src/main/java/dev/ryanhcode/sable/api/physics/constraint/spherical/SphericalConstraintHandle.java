b281c776d680ef985c87a0904120a3224e072ee0
한현빈 <tmvkrpxl0@proton.me>
2026-05-14 01:09:14 +0900

    upload wip

---
+++ common/src/main/java/dev/ryanhcode/sable/api/physics/constraint/spherical/SphericalConstraintHandle.java
@@ -0,0 +1,6 @@
+package dev.ryanhcode.sable.api.physics.constraint.spherical;
+
+import dev.ryanhcode.sable.api.physics.constraint.PhysicsConstraintHandle;
+
+public interface SphericalConstraintHandle extends PhysicsConstraintHandle {
+}
